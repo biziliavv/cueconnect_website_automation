5d05404bdaa18472f33e941e839fcda14d405138
vitaliybizilia <biziliavv@gmail.com>
2017-03-06 16:20:49 +0200

    Benefits page

---
+++ src/test/java/pageobjects/BenefitsPage.java
@@ -0,0 +1,7 @@
+package pageobjects;
+
+/**
+ * Created by vitaliybizilia on 3/6/17.
+ */
+public class BenefitsPage {
+}
---
+++ src/test/java/tests/BenefitsPageTests.java
@@ -0,0 +1,7 @@
+package tests;
+
+/**
+ * Created by vitaliybizilia on 3/6/17.
+ */
+public class BenefitsPageTests {
+}
